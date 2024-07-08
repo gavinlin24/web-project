ddda338f784c0a61a936f44c4d532549477ab103
gavinlin24 <gavinlin24@gmail.com>
2024-07-07 22:04:18 -0700

    userController bug fixes
    fixed bugs for handling requests for the user routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt')
 const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find().select('-password').lean()
 
-    if (!users) {
+    if (!users?.length) {
         return res.status(400).json({ message: "No users found" })
     }
 
@@ -46,7 +46,32 @@ const createUser = asyncHandler(async (req, res) => {
 //@route PATCH /users
 //@access Private
 const updateUser = asyncHandler(async (req, res) => {
-    //to be created
+    const { id, username, password } = req.body
+
+    if(!id || !username) {
+        return res.status(400).json({ message: "All fields are required" })
+    }
+
+    const user = await User.findById(id).exec()
+
+    if(!user) {
+        return res.status(400).json({ message: "User not found" })
+    }
+
+    const duplicate = await User.findOne({ username }).lean().exec()
+
+    if (duplicate && (duplicate._id.toString() !== id)) {
+        return res.status(409).json({ message: "Duplicate username" })
+    }
+
+    user.username = username
+    
+    if (password) {
+        user.password = await bcrypt.hash(password, 10)
+    }
+
+    const updatedUser = await user.save()
+    res.json({ message: `${updatedUser.username} updated`})
 })
 
 //@desc Delete a user
@@ -66,8 +91,8 @@ const deleteUser = asyncHandler(async (req, res) => {
     }
 
     const result = await user.deleteOne()
-    const reply = `User ${username} with ID ${result.id} deleted`
-
+    console.log(result._id)
+    const reply = `User ${user.username} with ID ${user._id} deleted`
     res.json(reply)
 })
 
